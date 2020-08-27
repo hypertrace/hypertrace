750748eb78553ab0d6ba6e720c77a0774a9c6262
José Carlos Chávez <jcchavezs@gmail.com>
2020-08-27 09:44:49 +0200

    chore(ci): tweaks to the tests.sh script to improve feedback.

--- .circleci/tests.sh
+++ .circleci/tests.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-HOST=${1:-127.0.0.1}
+# This script runs a quick E2E test against a running set of services described in ../docker-compose.yml
+# and ../docker-compose-zipkin-example.yml.
+# 
+# Usage:
+#
+# docker-compose -f docker/docker-compose.yml -f docker/docker-compose-zipkin-example.yml up -d
+# sh ./circleci/tests.sh
 
-curl --retry-connrefused --retry-max-time 60 "http://${HOST}:2020/" > /dev/null
+TRACES_SERVER_HOST=${1:-127.0.0.1}
+FRONTEND_SERVICE_HOST=${2:-127.0.0.1}
 
-curl -i http://${HOST}:8081
+echo ""
+echo "Making sure the traces service is up..."
+echo ""
+curl -o /dev/null --retry-connrefused --retry-max-time 60 "http://$TRACES_SERVER_HOST:2020" || { echo "Host $TRACES_SERVER_HOST is down." ; exit 1; }
 
-curl http://${HOST}:2020/graphql -i -H 'Content-Type: application/json' --data-binary \
+echo ""
+echo "Calling the frontend to generate a trace..."
+echo ""
+curl -o /dev/null -s http://${FRONTEND_SERVICE_HOST}:8081  || { echo "Host $FRONTEND_SERVICE_HOST is down." ; exit 1; }
+
+echo "Retrieving the list of traces:"
+echo ""
+
+TRACES=$(curl http://${TRACES_SERVER_HOST}:2020/graphql -H 'Content-Type: application/json' --data-binary \
 '{"query":"{
   traces(
     type: API_TRACE
@@ -19,4 +37,11 @@ curl http://${HOST}:2020/graphql -i -H 'Content-Type: application/json' --data-b
       id
     }
   }
-}"}'
+}"}')
+
+ERROR=$(echo $TRACES | jq -r .errors[0].message)
+if [[ "$ERROR" != "" ]]; then
+  echo ""
+  echo "Error while retrieving traces: $ERROR";
+  exit 1;
+fi
