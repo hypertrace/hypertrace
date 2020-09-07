df50efbcae65c724ae74713ef0b7e24cf099a9f9
José Carlos Chávez <jcchavezs@gmail.com>
2020-09-07 10:48:36 +0200

    chore(ci): adds retries to the tests and better error feedback to user.

--- .circleci/tests.sh
+++ .circleci/tests.sh
@@ -13,10 +13,9 @@ FRONTEND_SERVICE_HOST=${2:-127.0.0.1}
 
 echo ""
 echo "Making sure the traces service is up..."
-echo ""
 
 # curl --retry-connrefused is only available since 7.52.0.
-NUMBER_OF_RETRIES=5
+NUMBER_OF_RETRIES=10
 RETRY_COUNT=0
 while : ; do
   if [[ "$RETRY_COUNT" == "$NUMBER_OF_RETRIES" ]]; then
@@ -24,6 +23,9 @@ while : ; do
     exit 1
   fi
 
+  if [[ "$RETRY_COUNT" != "0" ]]; then
+    echo "Retry number $RETRY_COUNT."
+  fi
   (curl -s -o /dev/null "http://$TRACES_SERVER_HOST:2020") && break
   RETRY_COUNT=$((RETRY_COUNT+1)) 
   sleep 3
@@ -34,32 +36,56 @@ echo "Calling the frontend to generate a trace..."
 echo ""
 curl -o /dev/null -s http://${FRONTEND_SERVICE_HOST}:8081  || { echo "Host $FRONTEND_SERVICE_HOST is down." ; exit 1; }
 
-echo "Retrieving the list of traces:"
+echo "Retrieving the list of traces."
 echo ""
 
-TRACES=$(curl http://${TRACES_SERVER_HOST}:2020/graphql -H 'Content-Type: application/json' --data-binary \
-'{"query":"{
-  traces(
-    type: API_TRACE
-    between: {
-      startTime: \"2020-01-01T00:00:00.000Z\"
-      endTime: \"2025-01-01T00:00:00.000Z\"
-    }
-  ) {
-    results {
-      id
-    }
-  }
-}"}')
+NUMBER_OF_RETRIES=5
+RETRY_COUNT=0
+TRACES=""
+while : ; do
+  if [[ "$RETRY_COUNT" == "$NUMBER_OF_RETRIES" ]]; then
+    echo "Failed to retrieve traces from $TRACES_SERVER_HOST:2020 after $NUMBER_OF_RETRIES retries."
+    exit 1
+  fi
 
-ERROR=$(echo $TRACES | jq -r .errors[0].message)
-if [[ "$ERROR" != "null" ]]; then
-  echo ""
-  echo "Error while retrieving traces: $ERROR.";
-  exit 1;
-fi
+  if [[ "$RETRY_COUNT" != "0" ]]; then    
+    echo "Retry number $RETRY_COUNT."
+  fi
 
-TRACE_ID=$(echo $TRACES | jq -r .data.traces.results[0].id)
+  TRACES=$(curl http://${TRACES_SERVER_HOST}:2020/graphql -H 'Content-Type: application/json' --data-binary \
+  '{"query":"{
+    traces(
+      type: API_TRACE
+      between: {
+        startTime: \"2020-01-01T00:00:00.000Z\"
+        endTime: \"2025-01-01T00:00:00.000Z\"
+      }
+    ) {
+      results {
+        id
+      }
+    }
+  }"}')
+
+  ERROR=$(echo $TRACES | jq -r .errors[0].message)
+  if [[ "$ERROR" != "null" ]]; then
+    echo ""
+    echo "Error while retrieving traces: $ERROR.";
+    exit 1;
+  fi
+
+  TRACES_COUNT=$(echo $TRACES | jq '.data.traces.results | length')
+  if [[ "$TRACES_COUNT" == "0" ]]; then
+    echo $TRACES | jq .
+  else
+    TRACE_ID=$(echo $TRACES | jq -r .data.traces.results[0].id)
+    break
+  fi
+
+  RETRY_COUNT=$((RETRY_COUNT+1)) 
+  sleep 3
+done
+
 echo ""
-echo "Found trace $TRACE_ID."
+echo "Found trace \"$TRACE_ID\"."
 exit 0;
