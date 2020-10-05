3712ed7491ac62ee20a19b965df7cb0b36730b63
Adrian Cole <adriancole@users.noreply.github.com>
2020-10-05 12:28:03 +0800

    Removes orphaned scripts (#80)

--- docker/scripts/start-bootstrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-BOOTSTRAP_CMD="java -cp /app/resources:/app/classes:/app/libs/* org.hypertrace.core.bootstrapper.ConfigBootstrapper -c /app/resources/configs/config-bootstrapper/application.conf -C /app/resources/configs/config-bootstrapper/ --upgrade"
-exec $BOOTSTRAP_CMD
