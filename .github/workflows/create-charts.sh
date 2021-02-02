a2c6001e0d90297d374ca0a3966a66414600cfcf
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2021-02-02 18:34:20 +0530

    ci: add step to create RELEASE_NOTES.md file

--- .github/workflows/create-charts.sh
+++ .github/workflows/create-charts.sh
@@ -75,14 +75,54 @@ update_platform_services_charts() {
         echo '    condition: kafka-topic-creator.enabled'
 } 
 
+write_to_tmp_source() {
+        echo 'attribute-service:'$attribute_service_version
+        echo 'gateway-service:'$gateway_service_version
+        echo 'query-service:'$query_service_version
+        echo 'entity-service:'$entity_service_version
+        echo 'config-service:'$config_service_version
+        echo 'hypertrace-graphql:'$hypertrace_graphql_version
+        echo 'hypertrace-ui:'$hypertrace_ui_version
+        echo 'hypertrace-ingester:'$hypertrace_ingester_version
+}
+
+write_changelog() {
+    echo '### Attribute service'
+    changelog -m hypertrace attribute-service $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^attribute-service:" | cut -d":" -f2-)
+    echo '### Gateway service' 
+    changelog -m hypertrace gateway-service $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^gateway-service:" | cut -d":" -f2-)
+    echo '### Query service' 
+    changelog -m hypertrace query-service $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^query-service:" | cut -d":" -f2-)
+    echo '### Entity service' 
+    changelog -m hypertrace entity-service $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^entity-service:" | cut -d":" -f2-)
+    echo '### Config service' 
+    changelog -m hypertrace config-service $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^config-service:" | cut -d":" -f2-)
+    echo '### Hypertrace GraphQL service' 
+    changelog -m hypertrace hypertrace-graphql $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^hypertrace-graphql:" | cut -d":" -f2-)
+    echo '### Hypertrace UI' 
+    changelog -m hypertrace hypertrace-ui $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^hypertrace-ui:" | cut -d":" -f2-)
+    echo '### Hypertrace Ingester' 
+    changelog -m hypertrace hypertrace-ingester $(cat "kubernetes/tmp/compatibility_matrix.txt" | grep "^hypertrace-ingester:" | cut -d":" -f2-)
+}
 
 if [ $1 == "data-services" ]; then
     OUT_DIR="kubernetes/data-services"
     charts_file="${OUT_DIR}/Chart.yaml"
     update_data_services_charts > "${charts_file}"
+fi
 
-elif [ $1 == "platform-services" ]; then
+if [ $1 == "platform-services" ]; then
     OUT_DIR="kubernetes/platform-services"
     charts_file="${OUT_DIR}/Chart.yaml"
     update_platform_services_charts > "${charts_file}"
+fi
+
+if [ $1 == "release-notes" ]; then
+    release_notes_file="release_notes.md"
+    write_changelog > "${release_notes_file}"
+fi
+
+if [ $1 == "compatibility-matrix" ]; then
+    compatibilty_matrix_file="kubernetes/tmp/compatibility_matrix.txt"
+    write_to_tmp_source > "${compatibilty_matrix_file}"
 fi
