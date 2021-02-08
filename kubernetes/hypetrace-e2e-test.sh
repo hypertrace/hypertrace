a5fae7a236c3380343e100887dfaa342c148a42e
Ronak <ronak@traceable.ai>
2021-02-08 15:50:20 +0530

    Updated hypertrace e2e test script and its location

--- kubernetes/hypetrace-e2e-test.sh
+++ kubernetes/hypetrace-e2e-test.sh
@@ -1,8 +1,14 @@
-# This script will install Hypertrace on minikube, run e2e tests and clean your setup once done
-git checkout update-helm-charts
-minikube start --driver=none
+#!/bin/bash
+# This script helps in testing hypertrace setup e2e. It is currently mainly used for
+# release verification. if HT_ENV is set to minikub, it runs with https://minikube.sigs.k8s.io/docs/start/
+set -eEu -o functrace
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ ${HT_EVN} != "minikube" ]; then
+  minikube start --driver=none
+fi
+
 kubectl create ns hypertrace
-cd kubernetes
 ./hypertrace.sh install --clean
 
 kubectl get pods -n hypertrace
@@ -10,14 +16,13 @@ kubectl get services -n hypertrace
 sleep 40 # you can decrease it but never increase it
 kubectl get services -n hypertrace
 
-cd .github/graphql-e2e-tests/
+cd $SCRIPT_DIR/../.github/graphql-e2e-tests/
 ./ingest-traces.sh
 
 export HTUI_IP=$(kubectl get service hypertrace-ui -n hypertrace -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 ./gradlew run
 
-cd 
-cd .github/ui-e2e-tests/
+cd $SCRIPT_DIR/../.github/ui-e2e-tests/
 
 npm install 
 npm audit fix
@@ -25,24 +30,10 @@ CHROMEDRIVER_RELEASE="$(google-chrome --product-version)" && npm run install-web
 export HTUI_IP=$(kubectl get service hypertrace-ui -n hypertrace -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 npx protractor protractor.conf.js --suite smoke --baseUrl "http://${HTUI_IP}:2020"
 
-cd 
-cd kubernetes
+cd $SCRIPT_DIR
+./hypertrace.sh uninstall
 
-echo "Choose an option to continue"
-
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes )
-            ./hypertrace.sh uninstall
-            echo "[INFO] Uninstall successful."
-            break;;
-
-        No )
-            echo "[INFO] Uninstall cancelled."
-            exit;;
-    esac
-done
-;;
-
-minikube stop
-minikube delete --all
+if [ ${HT_EVN} != "minikube" ]; then
+  minikube stop
+  minikube delete --all
+fi
