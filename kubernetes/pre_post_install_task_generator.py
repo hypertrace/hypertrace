48315c913bbc43d0ca4adb7ab3ca9d09b18ad91b
Jayesh Bapu Ahire <jayesh.ahire@traceable.ai>
2021-03-01 10:37:28 +0530

    fix: replaces RE with yaml parsing for hook based manifest creation (#205)

--- kubernetes/pre_post_install_task_generator.py
+++ kubernetes/pre_post_install_task_generator.py
@@ -1,32 +1,26 @@
 import re
+import yaml
 
-def create_pre_hook_manifests(source_file, target_file):
-    with open (source_file) as manifest_file:
+def create_hook_based_manifests(hook, source_file, target_file):
+    hook_manifests=open(target_file, 'w')
+    with open (source_file, 'r') as manifest_file:
         content = manifest_file.read()
     helm_hooks = re.search(r'(?<=HOOKS:).*(?=MANIFEST)', content, re.DOTALL).group()
-    helm_pre_hooks = re.sub(r"#\s(Source)(.*)(?:config-bootstrapper)(.+)((?:\n.+)+)\n+.+\n.+", "", helm_hooks)
-    with open(target_file, "w") as pre_hooks:
-        pre_hooks.write(helm_pre_hooks)
-
-def create_post_hook_manifests(source_file, target_file):
-    with open (source_file) as manifest_file:
-        content = manifest_file.read()
-    post_install_hooks = re.search(r"#\s(Source)(.*)(?:config-bootstrapper)(.+)((?:\n.+)+)\n+.+\n", content).group()
-    with open(target_file, "w") as post_hooks:
-        post_hooks.write(post_install_hooks)
+    docs = yaml.load_all(helm_hooks, Loader=yaml.FullLoader)
+    for doc in docs:
+        if (hook in doc["metadata"]["annotations"]["helm.sh/hook"]):
+            hook_manifests.writelines("---\n")
+            yaml.dump(doc, hook_manifests)
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('hooks', help='pre or post hook manifests')
+    parser.add_argument('hook', help='pre or post hook manifests')
     parser.add_argument('source_file', help='The path of the manifest source file')
     parser.add_argument('target_file', help='The path to store result manifests')
     args = parser.parse_args()
-    if args.hooks == "pre":
-        create_pre_hook_manifests(args.source_file, args.target_file)
-    elif args.hooks == "post":
-        create_post_hook_manifests(args.source_file, args.target_file)
-    print("manifests created successfully!")
+    create_hook_based_manifests(args.hook, args.source_file, args.target_file)
+    print(args.hook, "manifests created successfully!")
     
 if __name__ == '__main__':
     main()
